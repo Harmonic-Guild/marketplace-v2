4192ee5e26e341b3efe9d43b549df54a2cd46e91
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-06-20 15:38:43 +0100

    fixes

--- components/WeeklyNft.tsx
+++ components/WeeklyNft.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import { BsCircle, BsStars } from "react-icons/bs";
 import { BiShareAlt } from "react-icons/bi";
+import Link from "next/link";
 
 const image = [
     {src: 'https://arweave.net/Yjn-nuWnEv8IgiFsw1LPKq1xjfa86yC2WVheWGPpixg', title: 'Buster character color'},
@@ -42,12 +43,11 @@ export default class WeeklyNft extends Component {
     var settings = {
       dots: false,
       className: "center",
-      centerMode: true,
       infinite: true,
       centerPadding: "1px",
       slidesToShow: 3,
       speed: 700,
-      slidesToScroll: 4,
+      slidesToScroll: 1,
       initialSlide: 0,
     //   nextArrow: <SampleNextArrow />,
     //   prevArrow: <SamplePrevArrow />,
@@ -79,25 +79,27 @@ export default class WeeklyNft extends Component {
       ]
     };
     return (
-      <div className="w-full h-screen pt-10 lg:px-32 px-12 bg-gray-200">
+      <div className="w-full h-full pt-10 lg:px-32 px-12 ">
         <div className=" text-center  font-bold text-gray-900 ">
             <h1 className="justify-center flex text-yellow-400">Lorem <BsStars className="w-6 h-6"/></h1>
-            <h2 className="text-3xl">NFTs of the week </h2>
+            <h2 className="lg:text-3xl text-xl">NFTs of the week </h2>
         </div>
         <Slider {...settings}>
-            {image.map(image => (
-                <div className="p-2 ">
+            {image.map((image, index) => (
+                <div key={index} className="p-2 ">
                     <div className="rounded-3xl p-2 border border-yellow-600">
                         <img src={image.src} alt="" className="px-2 rounded-2xl"/>
                         <div className="px-2">
-                            <p className="text-gray-900 font-bold text-2xl">{image.title}</p>
-                            <span className="text-lg font-medium text-stone-900">starts in: </span>
+                            <p className="text-gray-900 font-bold lg:text-2xl text-sm">{image.title}</p>
+                            <span className="lg:text-lg text-sm font-medium text-stone-900">starts in: </span>
                         </div>
                         <div className="flex justify-around">
-                            <button className="bg-yellow-500 py-4 px-6 rounded-xl w-1/2 text-center text-gray-900 font-medium text-lg">Buy</button>
+                            <Link href="/thing_id" passHref>
+                              <button className="bg-yellow-500 py-4 px-6 rounded-xl w-1/2 text-center text-gray-900 font-medium lg:text-lg text-xs">Buy</button>
+                            </Link>
                             <div className=' text-yellow-300'>
-                                <BsCircle className="relative h-12 w-12"/>
-                                <BiShareAlt className='absolute h-8 w-8 -mt-10 ml-2 '/>
+                                <BsCircle className="relative lg:h-12 h-8 lg:w-12 w-9"/>
+                                <BiShareAlt className='absolute lg:h-8 h-6 lg:w-8 w-6 lg:-mt-10 -mt-7 lg:ml-2 ml-1 '/>
                             </div>
                         </div>
                     </div>
