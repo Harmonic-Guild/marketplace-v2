ec24f2481010682253fed6d964eb8a3b1cad77ce
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-06-20 17:06:36 +0100

    dynamically passing header and footer to pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,18 @@ import "slick-carousel/slick/slick-theme.css";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+    <Header/>    
+    <Component {...pageProps} />
+    <Footer/>
+
+    </>
   )
 }
 
