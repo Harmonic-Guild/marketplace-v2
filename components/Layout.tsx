7453229979b19cee96bfd045aa5b086cc49c6528
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-06-20 15:39:11 +0100

    added footer comp

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 // import Footer from './Footer';
 import Header from './Header';
+import Footer from './Footer';
 
 const Layout = ({ children } : any) => {
   return (
     <>
         <Header />
         {children}
-        {/* <Footer/> */}
+        <Footer/>
     </>
   )
 }
