cebf26ab6b53e1437a50c8981454692f30e402ec
MZ Marshall <mzmarshall1278@gmail.com>
2022-06-21 14:51:02 +0100

    installed mintbase and configured mintbase, apollo and co

---
+++ interfaces/wallet.interface.ts
@@ -0,0 +1,16 @@
+import { Network, Chain, Wallet } from 'mintbase';
+import { ReactNode } from 'react'
+
+export interface Store {
+    id: string
+    name: string
+    symbol: string
+    baseUri: string
+    owner: string
+}
+
+export interface Thing {
+    id: string
+    metaId: string
+    memo: string
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ApolloProvider } from '@apollo/client'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
@@ -5,14 +6,22 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { WalletProvider } from "../services/providers/MintbaseWalletContext";
+import { useApollo } from '../services/apolloClient';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps)
   return (
     <>
-    <Header/>    
-    <Component {...pageProps} />
-    <Footer/>
+    <WalletProvider apiKey={process.env.NEXT_PUBLIC_MINTBASEJS_API_KEY || ''}>
+      <ApolloProvider client={apolloClient}>
+        <Header/>    
+        <Component {...pageProps} />
+        <Footer/>
+      </ApolloProvider>
+    </WalletProvider>
+    
 
     </>
   )
