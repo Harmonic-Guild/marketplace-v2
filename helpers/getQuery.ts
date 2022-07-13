ea932f17ebf4ec9593f87441d37f43b37060f165
MZ Marshall <mzmarshall1278@gmail.com>
2022-07-13 17:16:04 +0100

    testing query structure change

--- helpers/getQuery.ts
+++ helpers/getQuery.ts
@@ -20,24 +20,24 @@ const handlePriceRange = (range: string) => {
 
     switch (range) {
         case '1-5':
-            priceRangeFilter = {list: {_or: {price: {_lte: parseNearAmount("5")}, offer: {price: {_lte: parseNearAmount("5")}}}}}
+            priceRangeFilter = {lt: parseNearAmount("5"), gt: parseNearAmount("0")}
             
             break;
         case '5-10':   
-            priceRangeFilter =  {list: {_or: {price: {_gte: parseNearAmount("5"), _lte: parseNearAmount("10")}, offer: {price: {_gte: parseNearAmount("5"), _lte: parseNearAmount("10")}}}}}
+            priceRangeFilter =  {lt: parseNearAmount("10"), gt: parseNearAmount("5")}
 
             break;
         case '10-100':
-            priceRangeFilter = {list: {_or: {price: {_gte: parseNearAmount("10"), _lte: parseNearAmount("100")}, offer: {price: {_gte: parseNearAmount("10"), _lte: parseNearAmount("100")}}}}}
+            priceRangeFilter = {lt: parseNearAmount("100"), gt: parseNearAmount("10")}
 
             break;
 
         case '100+':
-            priceRangeFilter =  {list: {_or: {price: {_gt: parseNearAmount("100")}, offer: {price: {_gt: parseNearAmount("100")}}}}}
+            priceRangeFilter =  {lt: parseNearAmount("9999999999"), gt: parseNearAmount("100")}
 
             break;
         default: 
-            priceRangeFilter = {}
+            priceRangeFilter = {lt:parseNearAmount("999999999"), gt: parseNearAmount("0")}
     }
 
     return priceRangeFilter;
