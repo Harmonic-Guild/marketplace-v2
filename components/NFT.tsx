b6edfd21f126cc597911e982234d881fda29c580
MZ Marshall <mzmarshall1278@gmail.com>
2022-07-18 14:39:22 +0100

    removed extra fetch and fixed bid and buy showing

--- components/NFT.tsx
+++ components/NFT.tsx
@@ -7,34 +7,10 @@ import {formatNearAmount} from 'near-api-js/lib/utils/format'
 import Image from 'next/image'
 import { AiOutlineRight } from 'react-icons/ai'
 
-function NFT({token, baseUri}: any) {
+function NFT({token}: any) {
     
-    const {thing, list} = token;
-    
-    const [metadata, setMetadata] =
-      useState<{
-        [key: string]: string
-      } | null>(null)
-  
-    const fetchMetadata = async (url: string) => {
-      
-      const response = await fetch(url)
-  
-      const result = await response.json()
-  
-      if (!result) return
-  
-      setMetadata(result)
-      
-    }
-  
-    useEffect(() => {
-      fetchMetadata(`${baseUri}/${thing.metaId}`)
-      
-      
-    }, [])
-  
-    if (!metadata) return null
+    const {thing, lists} = token;
+    const list = lists[0];
     
     
     return ( 
@@ -42,7 +18,7 @@ function NFT({token, baseUri}: any) {
             <div className="lg:w-full md:w-5/6  border border-mp-brown-2 rounded-2xl bg-mp-peach-2 cursor-pointer">
                 <div className="p-3">
                     
-                        {metadata.animation_hash ? (
+                        {(thing.metadata.animation_type !== null && thing.metadata.animation_type !== 'image/jpeg' && thing.metadata.animation_type !== 'image/png' ) ? (
                             // <video controls className='object-contain mx-auto rounded-lg' poster={metadata.media} controlsList="nodownload" muted>
                             //     <source src={metadata.animation_url} ></source>
                             // </video>
@@ -52,7 +28,7 @@ function NFT({token, baseUri}: any) {
                                 height={500}
                                 width={500}
                                 objectFit="cover"
-                                src={metadata.media}
+                                src={thing.metadata.media}
                                 alt={'alt'} />
                                 
                                 <div className='absolute w-7 h-7 rounded-lg bg-gray-900 top-1/2 left-1/2 text-white  cursor-pointer'>play</div>
@@ -64,7 +40,7 @@ function NFT({token, baseUri}: any) {
                                 height={500}
                                 width={500}
                                 objectFit="cover"
-                                src={metadata.media}
+                                src={thing.metadata.media}
                                 alt={'alt'} />
                             </div>
                         )}
@@ -76,19 +52,19 @@ function NFT({token, baseUri}: any) {
                                 <div className='bg-blue-700 rounded-full h-7 w-7 absolute right-7 text-white p-1'>RR</div>
                                 <div className='bg-green-700 rounded-full h-7 w-7 absolute right-2 text-white p-1'>SM</div>
                             </div>
-                            <div className="font-semibold my-1 py-1 text-sm">{metadata.title}</div>
+                            <div className="font-semibold my-1 py-1 text-sm">{thing.metadata.title}</div>
                             <div className="flex my-1 py-1 justify-between">
                                 <p className='flex'>
-                                    {list? list?.offer?.price?
+                                    {list? !list?.autotransfer?
                                      (<>
-                                     Last Bid: {formatNearAmount(Number(list?.offer?.price).toLocaleString('fullwide', { useGrouping: false }),5)}
+                                     Last Bid: {formatNearAmount(Number(list?.offer?.price || 0).toLocaleString('fullwide', { useGrouping: false }),5)}
                                      <span className='pt-1  ml-1'><Near></Near></span>
                                     </>):
                                     (<>
                                         Price: {formatNearAmount(Number(list?.price).toLocaleString('fullwide', { useGrouping: false }),5)}
                                         <span className='pt-1  ml-1'><Near></Near></span>
                                        </>): 
-                                    `Not Available`
+                                    `Not Available ${list?.price}`
                                     }
                                     </p>
                                 <div className="md:flex hidden relative">
@@ -101,7 +77,7 @@ function NFT({token, baseUri}: any) {
                                 <button className='flex action-btn '>
                                     <span className=''>
                                     {
-                                    list? list?.offer?.price? 'Bid': 'Get Details' : 'N/A'
+                                    list? !list?.autotransfer ? 'Bid': 'Get Details' : 'N/A' 
                                     }
                                     </span> 
                                      <span className='border-l border-black  md:ml-2 ml-0'><AiOutlineRight className=' w-6 h-6'/></span> 
