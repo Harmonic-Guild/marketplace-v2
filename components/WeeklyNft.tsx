5858788ccb901eb0356fd10fb13d8d065d0be0c0
MZ Marshall <mzmarshall1278@gmail.com>
2022-07-19 15:06:26 +0100

    updated query and passed NFT

--- components/WeeklyNft.tsx
+++ components/WeeklyNft.tsx
@@ -10,16 +10,33 @@ import { BiArrowFromLeft, BiArrowFromRight } from "react-icons/bi";
 import { gql } from "apollo-boost";
 import { useLazyQuery } from "@apollo/client";
 import Image from 'next/image';
+import NFT from "./NFT";
 
 const FETCH_WEEKLY = gql`
 query MyQuery($storeId: String!) {
-  token(where: {storeId: {_eq: $storeId}, burnedAt: {_is_null: true}}, limit: 5, distinct_on: thingId, order_by: {thingId: desc}) {
+  token(where: {storeId: {_eq: $storeId}, list: {price: {_is_null: false}}, burnedAt: {_is_null: true}}, limit: 4, distinct_on: thingId, order_by: {thingId: desc}) {
     id
+    lists(
+      order_by: {createdAt: desc}, limit: 1
+    ){
+      price
+      autotransfer
+      offer{
+        price
+        timeout
+      }
+    }
+    txId
+    thingId
     thing {
       id
       metaId
-      metadata {
+      metadata{
+        title
         media
+        media_type
+        animation_url
+        animation_type
       }
     }
   }
@@ -27,31 +44,8 @@ query MyQuery($storeId: String!) {
 `
 
 
-// function SampleNextArrow(props: any) {
-//     const { className, style, onClick } = props;
-//     return (
-//       <div
-//         className={className}
-//         style={{ ...style, display: "block", background: "red" }}
-//         onClick={onClick}
-//       >
-        
-//       </div>
-//     );
-// }
 
-// function SamplePrevArrow(props: any) {
-//     const { className, style, onClick } = props;
-//     return (
-//       <div
-//         className={className}
-//         style={{ ...style, display: "block", background: "green" }}
-//         onClick={onClick}
-//       />
-//     );
-//   }
-
- const  WeeklyNft = () => {
+ const  WeeklyNft = ({storeId}: {storeId: string}) => {
 
   const [tokens, setTokens] = useState<[]>([])
 
@@ -104,7 +98,7 @@ query MyQuery($storeId: String!) {
      useEffect(() => {
       getTokens({
         variables: {
-          storeId: process.env.NEXT_PUBLIC_STOREID!,
+          storeId,
         },
       })
     }, []);
@@ -116,11 +110,11 @@ query MyQuery($storeId: String!) {
   
       if (tokensData?.token.length === 0) return;
   
-      const weeklyTokens = tokensData.token.map((token: {}) => token)
+      const weeklyTokens = tokensData.token.map((token: any) => token)
 
-      setTokens(weeklyTokens);     
-      
-      console.log(tokens);
+      setTokens(weeklyTokens);    
+
+      console.log(tokensData, '*/*//*/*//+896');
       
       
     }, [tokensData])
@@ -131,38 +125,11 @@ query MyQuery($storeId: String!) {
         <p className='text-mp-orange-1 mb-2'>Lorem <Vector className='inline'></Vector></p>
             <h2 className="text-mp-dark-2 text-4xl font-bold">NFTs of the week </h2>
         </div>
-        <Slider {...settings}>
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 w-full pt-4 gap-y-5 gap-2">
             {tokens.map((token:any) => (
-                <div key={token.id} className="p-2 ">
-                    <div className="rounded-2xl p-2 border border-yellow-600">
-                      <div className="h-64 w-fit rounded-lg shadow-xl relative overflow-hidden">
-                        <Image
-                          src={token.thing.metadata.media} 
-                          alt=""
-                          objectFit="cover"
-                          layout="fill"
-                        />
-                      </div>
-                        <div className="text-sm py-2 text-mp-dark-3 relative">
-                          <div className="timer">16:12:56 hrs</div>
-                          <div className="font-semibold my-3 py-1">Buster Character Color</div>
-                          <div className="flex my-0 py-1 justify-between">
-                              <p className='flex'>Last Bid: 0.25 <span className='mt-[.15rem] ml-1'><Near></Near></span></p>
-                              <div className="flex relative">
-                                  <div className='bg-red-700 rounded-full h-7 w-7 absolute right-12 p-1 text-white'>MZ</div>
-                                  <div className='bg-blue-700 rounded-full h-7 w-7 absolute right-7 text-white p-1'>RR</div>
-                                  <div className='bg-green-700 rounded-full h-7 w-7 absolute right-2 text-white p-1'>SM</div>
-                              </div>
-                          </div>
-                          <div className="flex mt-2 pt-1 justify-between px-2">
-                              <button className='flex action-btn'>Bid <span className='border-l border-black pl-2 ml-2'><Right></Right></span></button>
-                              <button><Share></Share></button>
-                          </div>
-                        </div>
-                    </div>
-                </div>
+                <NFT token={token} key={token.id} />
             ))}
-        </Slider>
+        </div>
       </div>
     );
   }
