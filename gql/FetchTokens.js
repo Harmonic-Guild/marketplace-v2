c529c1197ff60d6533746b29d73451dda44dd474
MZ Marshall <mzmarshall1278@gmail.com>
2022-10-20 15:04:24 +0100

    added fetch token query

--- gql/FetchTokens.js
+++ gql/FetchTokens.js
@@ -1,49 +1,43 @@
 import { gql } from "apollo-boost";
 
 export const fetchTokens = gql`
-query FetchTokensByStoreId ($storeId: String!, $order: order_by!, $type:[String!], $lt: numeric, $gt: numeric, $limit: Int, $offset: Int) {
-    metadata(
-        order_by: {thing: {createdAt: $order}},
-        limit: $limit,
-        offset: $offset,
-        where: {
-            animation_type: {
-                _in: $type
-              },
-            thing: {
-                storeId: {_eq: $storeId}
-                tokens: {
-                    list: {
-                        removedAt: {_is_null: true},
-                        price: {_gte: $gt, _lte: $lt}
-                    }
-                },
-            },
-        }
-    ){
+query FetchStore($storeId: String!, $limit: Int = 20, $offset: Int = 0){
+    store(where: { id: { _eq: $storeId } }){
         id
-        title
-        media
-        media_type
-        animation_url
-        animation_type
-        thing {
-            id
-            tokens(distinct_on: id, where: {list: {removedAt: {_is_null: true}}}){
-                txId
-                thingId
-                id
-                lists(order_by: {createdAt: desc}, limit: 1) {
-                    id
+        baseUri
+        owner
+        tokens(
+            order_by: { thingId: asc }
+            where: { storeId: { _eq: $storeId }, burnedAt: { _is_null: true } }
+            limit: $limit
+            offset: $offset
+            distinct_on: thingId
+          ){
+              id
+              lists(
+                order_by: {createdAt: desc}, limit: 1
+              ){
+                  price
+                  autotransfer
+                  offer{
                     price
-                    autotransfer
-                    offer{
-                        price
-                        timeout
-                    }
+                    timeout
+                  }
+              }
+              txId
+              thingId
+              thing{
+                id
+                metaId
+                metadata{
+                    title
+                    media
+                    media_type
+                    animation_url
+                    animation_type
                 }
-            }
-        }
+              }
+          }
     }
 }
 `
