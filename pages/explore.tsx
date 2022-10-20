213f88dfad090a878b98fe86d0c5c0ca6e9c76e1
MZ Marshall <mzmarshall1278@gmail.com>
2022-10-20 15:04:59 +0100

    imported fetch tokens

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -11,7 +11,7 @@ import QueryFilters from "../helpers/getQuery";
 // import Categories from "../components/category/Categories";
 // import Artists from "../components/category/Artists";
 // import Color from "../components/category/Color";
-// import { fetchTokens } from '../gql/FetchTokens'
+import { fetchTokens } from '../gql/FetchTokens'
 
 const FETCH_STORE = gql`
     query FetchStore($storeId: String!) {
@@ -25,46 +25,7 @@ const FETCH_STORE = gql`
     }
 `;
 
-const FETCH_TOKENS = gql`
-  query FetchStore($storeId: String!, $limit: Int = 20, $offset: Int = 0) {
-    store(where: { id: { _eq: $storeId } }) {
-      id
-      baseUri
-      owner
-      tokens(
-        order_by: { thingId: asc }
-        where: { storeId: { _eq: $storeId }, burnedAt: { _is_null: true } }
-        limit: $limit
-        offset: $offset
-        distinct_on: thingId
-      ) {
-        id
-      lists(
-        order_by: {createdAt: desc}, limit: 1
-      ){
-        price
-        autotransfer
-        offer{
-          price
-          timeout
-        }
-      }
-      txId
-      thingId
-      thing {
-        id
-        metaId
-        metadata{
-          title
-          media
-          media_type
-          animation_url
-          animation_type
-        }
-      }
-      }
-    }
-`;
+const FETCH_TOKENS = fetchTokens
 const explore = () => {
     const [store, setStore] = useState<Store | null>(null);
     // const [things, setThings] = useState<any>([])
@@ -137,6 +98,7 @@ const explore = () => {
         });
 
         setTokens(tokens);
+        
     }, [tokensData]);
 
     const setFilters = (filters: any) => {
@@ -155,7 +117,7 @@ const explore = () => {
                 <h2 className="text-mp-dark-2 text-4xl font-bold">Explore</h2>
             </div>
             <div>
-                <DropDown setFilters={setFilters} />
+                {/* <DropDown setFilters={setFilters} /> */}
             </div>
             <div className="xl:flex block justify-around">
                 {/* <div className=" order-last pt-4 col-span-1">
