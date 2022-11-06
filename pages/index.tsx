9c107c34348e78b8094866a0d5cddae94fa40794
Jaswinder Singh <jassmultani2@gmail.com>
2022-11-06 15:56:45 +0530

    added bug reporting via gleap

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 // import AboutArtist from "../components/AboutArtist";
+import Gleap from 'gleap';
+import { useEffect } from 'react';
 
 const FeaturedNft = dynamic(() => import("../components/FeaturedNft"));
 const WeeklyNft = dynamic(() => import("../components/WeeklyNft"));
@@ -9,6 +11,11 @@ const AboutArtist = dynamic(() => import("../components/AboutArtist"));
 
 const Home: NextPage = () => {
     const storeName = process.env.NEXT_PUBLIC_STORE_NAME!;
+
+    useEffect(() => {
+        // Run within useEffect to execute this code on the frontend.
+        Gleap.initialize("QiQEFx0bETLdNLtHD0n1dFu81ayhDck3");
+    }, []);
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
