6752ec30f6487f6d3b60bf0cb112037628cb8105
MZ Marshall <mzmarshall1278@gmail.com>
2022-11-16 14:31:22 +0100

    cleanup and fetched values from config

--- components/AboutArtist.tsx
+++ components/AboutArtist.tsx
@@ -1,12 +1,10 @@
-import { FC, useEffect, useState } from "react";
+import { FC, } from "react";
 import { GiStarShuriken } from "react-icons/gi";
 import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 import { MdVerified } from "react-icons/md";
-import { gql } from "apollo-boost";
-import { useLazyQuery } from "@apollo/client";
-import { Token } from "../constants/interfaces";
-import logo from "../assets/harmonic-logo.png";
 import Near from "../icons/near.svg";
+import config from '../config/config.json'
+
 
 import styles from "../styles/AboutArtist.module.scss";
 import Image from "next/image";
@@ -15,79 +13,10 @@ interface Props {
     storeId: string;
 }
 
-const FETCH_WEEKLY = gql`
-    query MyQuery($storeId: String!) {
-        token(
-            where: { storeId: { _eq: $storeId }, list: { price: { _is_null: false } }, burnedAt: { _is_null: true } }
-            limit: 3
-            offset: 3
-            distinct_on: thingId
-            order_by: { thingId: desc }
-        ) {
-            id
-            lists(order_by: { createdAt: desc }, limit: 1) {
-                price
-                autotransfer
-                offer {
-                    price
-                    timeout
-                }
-            }
-            txId
-            thingId
-            thing {
-                id
-                metaId
-                metadata {
-                    title
-                    media
-                    media_type
-                    animation_url
-                    animation_type
-                }
-            }
-        }
-    }
-`;
+const images = config.images
 
-const images = [
-    "https://pbs.twimg.com/media/FbpyI7oUcAAJtWy?format=jpg&name=small",
-    "https://pbs.twimg.com/media/Fbpz9sVXkAA6l2h?format=jpg&name=small",
-    "https://pbs.twimg.com/media/FbpAEs8VEAAlXCB?format=jpg&name=small",
-];
-
-const AboutArtist: FC<Props> = ({ storeId }) => {
-    const [tokens, setTokens] = useState<Token[]>([]);
-
-    const [getTokens, { loading: loadingtokensData, data: tokensData }] = useLazyQuery(FETCH_WEEKLY, {
-        variables: {
-            storeId: "",
-        },
-    });
-
-    useEffect(() => {
-        getTokens({
-            variables: {
-                storeId,
-            },
-        });
-    }, []);
-
-    useEffect(() => {
-        // console.log(storeData);
-
-        if (!tokensData) return;
-
-        if (tokensData?.token.length === 0) return;
-
-        const weeklyTokens = tokensData.token.map((token: any) => token);
-
-        setTokens(weeklyTokens);
-        console.log(weeklyTokens);
-
-        // console.log(tokensData, "*/*//*/*//+896");
-        // console.log(weeklyTokens[1].thing.metadata.media);
-    }, [tokensData]);
+const AboutArtist: FC<Props> = () => {
+   
 
     return (
         <div className={styles.container}>
@@ -101,19 +30,19 @@ const AboutArtist: FC<Props> = ({ storeId }) => {
                 <div className={styles.header}>
                     <div className={styles["header-image-cont"]}>
                         <Image
-                            src="https://pbs.twimg.com/media/FbnZWY1XwAAjUWk?format=jpg&name=small"
+                            src={config.logo3}
                             className="object-contain mx-auto rounded-lg"
                             layout="fill"
-                            objectFit="cover"
+                            objectFit="contain"
                             alt={"header"}
                         />
                     </div>
                     <div className={styles["mini-cont"]}>
                         <div className={styles.avatar}>
                             <Image
-                                src="https://pbs.twimg.com/media/Fbm9XtXXkAY-jxS?format=jpg&name=360x360"
+                                src={config.images[1]}
                                 layout="fill"
-                                objectFit="cover"
+                                objectFit="contain"
                                 alt={"avatar"}
                             />
                         </div>
@@ -126,7 +55,7 @@ const AboutArtist: FC<Props> = ({ storeId }) => {
                 <div className={styles["more-info"]}>
                     <div className={styles.top}>
                         <p>
-                            Artist name <MdVerified color="#1D9BF0" />
+                            {config.title} <MdVerified color="#1D9BF0" />
                         </p>
                         <button>Find out more &rarr;</button>
                     </div>
@@ -150,16 +79,14 @@ const AboutArtist: FC<Props> = ({ storeId }) => {
                 <div className={styles["org-info"]}>
                     <div className={styles["org-name-logo"]}>
                         <div className={styles["inner-logo-cont"]}>
-                            <Image src={logo.src} layout="fill" objectFit="contain" />
+                            <Image src={config.logo2} layout="fill" objectFit="contain" />
                         </div>
                     </div>
                     <div className={styles["more-org-info"]}>
                         <div className={styles["text-cont"]}>
-                            <p className={styles.name}>Harmonic Guild</p>
+                            <p className={styles.name}>{config.title}</p>
                             <p className={styles["mini-text"]}>
-                                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse hic ut eum. Non cumque laudantium similique,
-                                voluptatem accusamus iste nostrum veritatis tenetur minus deleniti. Quibusdam mollitia nobis corrupti facilis
-                                inventore."
+                                {config.description}
                             </p>
                         </div>
                         <div className={styles["info-cont"]}>
