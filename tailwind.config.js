205788c5ee17eb35672eb17175a4126c72014208
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-11-22 17:21:06 +0100

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,9 +22,9 @@ module.exports = {
         },
         extend: {
             colors: {
-                primary: "rgba(35, 50, 71, 1)",
-                secondary: "rgba(81, 116, 166, 1)",
-                card: "#c5bdf0",
+                primary: "#c20d0d",
+                secondary: "#661313",
+                card: "#cf9191",
                 "mp-dark": {
                     1: "#252B42",
                     2: "#161621",
