8d314a7df114fa5a9a78a35607919a6e1c34f54f
Babagana Abba <babagabba@rocketmail.com>
2022-11-25 17:20:55 +0100

    rectified build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
             md: "768px",
             // => @media (min-width: 768px) { ... }
 
-            lg: "1024px",
+            lg:"1024px",
             // => @media (min-width: 1024px) { ... }
 
             xl: "1280px",
