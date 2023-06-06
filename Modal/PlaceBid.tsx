c506bfdfc4e3395b394d8531c1fc22f0b87cbf40
MZ Marshall <mzmarshall1278@gmail.com>
2023-06-06 19:23:08 +0100

    handled close  modal

--- Modal/PlaceBid.tsx
+++ Modal/PlaceBid.tsx
@@ -1,42 +1,50 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Near from '../icons/near.svg'
 import { AiOutlineClose } from 'react-icons/ai';
 
-const PlaceBid = () => {
+import styles from "../styles/MakeOffer.module.scss";
 
-    const [cancleModal, setCancleModal] = useState(true)
-    const [showModal, setShowModal] = useState(false)
+const PlaceBid = ({buy, closeModal}: any) => {
 
-    const cancle = () => {
-        setCancleModal(false)   
-        
-    }
+    const [bid, setBid] = useState("0")
+
+    const handleChange = (e: any) => {
+        setBid(e.target.value);
+    };
+
   return (
-    <div>
-        {cancleModal &&
-        <div className="h-screen w-screen glass-morphism fixed top-0 left-0 z-40">
-            <div className="align-middle mx-auto md:w-2/5 w-5/6 relative top-80 lg:top-52 rounded-xl p-8 bg-white">
-                <div className='flex w-full justify-between'>
-                    <p className='text-xl font-bold'>Buster character color</p>
-                    {cancleModal && <span className='text-gray-400 border border-mp-brown-1 rounded-full p-2 cursor-pointer' onClick={cancle}><AiOutlineClose/></span>}
-                </div>
-                <div className="flex font-bold justify-between w-full lg:w-3/5">
-                    <div className="bg-gray-900 text-white rounded-md px-2 py-1">16:12:56 hrs</div>
-                    <span className='lg:text-sm text-lg'>Time remaining</span>
-                </div>
-                <div className='my-2 flex justify-between w-full lg:w-3/5 border border-gray-400 rounded-lg'>
-                    <input type="text" className='w-full'/> 
-                    <span className='border-l border-gray-400 p-2'><Near/></span>
-                </div>
+    <div className={`${styles["modal-cont"]} glass-morphism`}>
+                    <div className={`mx-auto w-4/5 relative top-80 lg:top-52 rounded-xl bg-white ${styles["inner-cont"]}`}>
+                        <div className="flex w-full justify-between">
+                            <p className="text-xl font-bold">Buster character color</p>
+                            <span
+                                className="text-gray-400 border border-mp-brown-1 rounded-full p-2 cursor-pointer"
+                                onClick={() => closeModal(false)}
+                            >
+                                <AiOutlineClose />
+                            </span>
+                        </div>
+                        <div className="flex font-bold justify-between w-full lg:w-3/5 my-4">
+                            <div className="bg-gray-900 text-white rounded-md px-2 py-1">16:12:56 hrs</div>
+                            <span className="lg:text-sm text-lg">Time remaining</span>
+                        </div>
+                        <div className="flex my-4 justify-between w-full lg:w-3/5 border border-gray-400 rounded-lg">
+                            <input type="number" className="w-full outline-none" min={0} value={bid} onChange={handleChange} />
+                            <span className="border-l border-gray-400 p-2">
+                                <Near className="w-4 h-4" fill="black" />
+                            </span>
+                        </div>
 
-                <div className="">
-                    <button onClick={() => setShowModal(false) } className="border-2 rounded-xl outline-none btnColor py-2 font-medium px-6 lg:px-12 text-gray-800">Place bid</button>                    
+                        <div className="">
+                            <button
+                                onClick={() => buy(bid)}
+                                className="border-2 rounded-xl outline-none btnColor py-2 font-medium px-6 lg:px-12 text-gray-800"
+                            >
+                                Place bid
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                
-            </div>
-        </div>
-}
-    </div>
   )
 }
 
