2d6dd117f693be3e1556d4423c49d8808292060e
MZ Marshall <mzmarshall1278@gmail.com>
2023-06-07 20:24:35 +0100

    (wip)

--- pages/thing/[thing_id].tsx
+++ pages/thing/[thing_id].tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { TbExternalLink } from "react-icons/tb";
+import React, { useState } from "react";
 import { BsChevronLeft } from "react-icons/bs";
 import { CgArrowsExpandRight } from "react-icons/cg";
 import Image from "next/image";
@@ -18,7 +17,7 @@ import { metadataByMetadataId } from "@mintbase-js/data";
 import { MetadataByMetadataIdQueryResult } from "@mintbase-js/data/lib/api/metadataByMetadataId/metadataByMetadataId.types";
 import { useWallet } from "@mintbase-js/react";
 
-const thing_id = ({ thing_id, data }: { thing_id: string, data:  MetadataByMetadataIdQueryResult | null | undefined }) => {
+const thing_id = ({ thing_id, data }: { thing_id: string; data: MetadataByMetadataIdQueryResult | null | undefined }) => {
     interface Thing {
         contract: {
             id: string;
@@ -53,28 +52,23 @@ const thing_id = ({ thing_id, data }: { thing_id: string, data:  MetadataByMetad
         createdAt: string | number;
     }
 
-    const [listings, setListings] = useState<any>([]);
-    const [metadata, setMetadata] = useState<any>([]);
-    const [tokenCount, setTokenCount] = useState<number>(0);
-    const {  isConnected } = useWallet();
+    const { isConnected } = useWallet();
     const [hide, setHide] = useState<boolean>(false);
     const [enlarge, setEnlarge] = useState(false);
 
-
-    useEffect(() => {
-        setMetadata(data?.metadata);
-        setTokenCount(data?.tokenCount.aggregate.count!);
-        setListings(data?.listings);
-    }, []);
+    const metadata = data?.metadata!;
+    const tokenCount = data?.tokenCount.aggregate.count!;
+    const listings = data?.listings!;
+    console.log(data);
+    
 
     const toggleDiscription = () => {
         setHide(!hide);
     };
 
     const buy = (bid: number) => {
-        const token_Id = listings[0]?.token?.token.id! + ":" + thing_id.split(":")[0];
-        const marketAddress = listings[0]?.market_id;
-
+        // const token_Id = listings[0]?.token?.token.id! + ":" + thing_id.split(":")[0];
+        // const marketAddress = listings[0]?.market_id;
         // if (listings[0]?.kind === "simple") {
         //     wallet?.makeOffer(token_Id, tokenPrice, { marketAddress });
         // } else {
@@ -89,19 +83,19 @@ const thing_id = ({ thing_id, data }: { thing_id: string, data:  MetadataByMetad
         useGrouping: false,
     });
 
-    let currentBid;
-    if (listings && !listings[0]?.offers) {
-        currentBid = "0";
-    } else {
-        currentBid =
-            (listings && formatNearAmount(Number(listings[0]?.offers[0]?.offer_price).toLocaleString("fullwide", { useGrouping: false }), 5)) || 0;
-    }
+    // let currentBid;
+    // if (listings && !listings[0]?.offers) {
+    //     currentBid = "0";
+    // } else {
+    //     currentBid =
+    //         (listings && formatNearAmount(Number(listings[0]?.offers[0]?.offer_price).toLocaleString("fullwide", { useGrouping: false }), 5)) || 0;
+    // }
 
-    const args = {
-        token_Id: listings && listings[0]?.token.id! + ":" + thing_id.split(":")[0],
-        marketAddress: listings && listings[0]?.market_id,
-        tokenPrice,
-    };
+    // const args = {
+    //     token_Id: listings && listings[0]?.token.id! + ":" + thing_id.split(":")[0],
+    //     marketAddress: listings && listings[0]?.market_id,
+    //     tokenPrice,
+    // };
 
     return (
         <>
@@ -158,91 +152,77 @@ const thing_id = ({ thing_id, data }: { thing_id: string, data:  MetadataByMetad
                         </div>
                         <div className="w-full text-font-color">
                             <div className="text-4xl font-bold mb-5 text-font-color">{metadata[0]?.title}</div>
+                        </div>
 
-                            <div className="text-lg">
-                                {metadata[0]?.contract?.created_at ? (
-                                    `_`
-                                ) : (
-                                    <div className="flex gap-3 text-font-color">
-                                        {`Minted On: ` + new Date(metadata[0]?.contract?.created_at!).toDateString()}{" "}
-                                        <TbExternalLink color={process.env.NEXT_PUBLIC_SECONDARY_COLOR} className="w-6 h-6" />
-                                    </div>
-                                )}
-                            </div>
-
-                            <div className="text-font-color">
-                                <div className="mt-10 border-b md:border-b-0 border-primary-color pb-4">
-                                    <div className="border-b border-primary-color mb-3 pb-3">
-                                        <span className="text-3xl font-bold text">Description</span>
-                                    </div>
-
-                                    <p className={hide ? "" : "line-clamp-3"}>{metadata[0]?.description}</p>
-                                    <span id="span" onClick={toggleDiscription} className="cursor-pointer text-blue-400 hover:underline">
-                                        {" "}
-                                        {!hide ? ".....see more" : "see less"}
-                                    </span>
+                        <div className="text-font-color">
+                            <div className="mt-10 border-b md:border-b-0 border-primary-color pb-4">
+                                <div className="border-b border-primary-color mb-3 pb-3">
+                                    <span className="text-3xl font-bold text">Description</span>
                                 </div>
+
+                                <p className={hide ? "" : "line-clamp-3"}>{metadata[0]?.description}</p>
+                                <span id="span" onClick={toggleDiscription} className="cursor-pointer text-blue-400 hover:underline">
+                                    {" "}
+                                    {!hide ? ".....see more" : "see less"}
+                                </span>
                             </div>
+                        </div>
 
-                            <div className="flex flex-col-reverse lg:flex-col text-font-color">
-                                <div className="flex flex-col-reverse items-center lg:flex-row mt-8 lg:gap-5 lg:justify-between">
-                                    <div className="">
-                                        <div className="flex items-center justify-between gap-3">
-                                            <p className="text-2xl font-bold">Details</p>
-                                            <span className="border-b px-12 lg:px-20 border-yellow-600 mx-2" />
-                                            <div className="border-2 border-secondary-color rounded-full p-2 px-3">
-                                                <a
-                                                    href={`https://explorer.testnet.near.org/transactions/${metadata[0]?.title}`}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                >
-                                                    <Near className="w-4 h-4" fill="black" />
-                                                </a>
-                                            </div>
-                                            <div className="border-2 border-secondary-color rounded-full p-1 px-3">
-                                                <a
-                                                    href={`https://viewblock.io/arweave/tx/${thing_id.split(":")[0]}`}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                >
-                                                    <div className="w-6 h-6">
-                                                        <Image src={Arweave} className="" />
-                                                    </div>
-                                                </a>
-                                            </div>
+                        <div className="flex flex-col-reverse lg:flex-col text-font-color">
+                            <div className="flex flex-col-reverse items-center lg:flex-row mt-8 lg:gap-5 lg:justify-between">
+                                <div className="">
+                                    <div className="flex items-center justify-between gap-3">
+                                        <p className="text-2xl font-bold">Details</p>
+                                        <span className="border-b px-12 lg:px-20 border-yellow-600 mx-2" />
+                                        <div className="border-2 border-secondary-color rounded-full p-2 px-3">
+                                            <a
+                                                href={`https://explorer.testnet.near.org/transactions/${metadata[0]?.title}`}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                <Near className="w-4 h-4" fill="black" />
+                                            </a>
                                         </div>
-
-                                        <div className="bg-primary-color rounded-lg my-8 py-2">
-                                            <p className="text-center text-font-color text-lg">{tokenCount} Tokens Minted</p>
+                                        <div className="border-2 border-secondary-color rounded-full p-1 px-3">
+                                            <a href={`https://viewblock.io/arweave/tx/${thing_id.split(":")[0]}`} target="_blank" rel="noreferrer">
+                                                <div className="w-6 h-6">
+                                                    <Image src={Arweave} className="" />
+                                                </div>
+                                            </a>
                                         </div>
                                     </div>
+
+                                    <div className="bg-primary-color rounded-lg my-8 py-2">
+                                        <p className="text-center text-font-color text-lg">{tokenCount} Tokens Minted</p>
+                                    </div>
                                 </div>
-                                {listings && listings.length ? (
+                            </div>
+                            {listings && listings.length ? (
                                     //&& (tokensData.listings[0].market_id === process.env.NEXT_PUBLIC_marketAddress)
                                     <div>
                                         {listings && listings[0]?.kind === "simple" ? (
                                             <PurchaseNft
-                                                args={args}
+                                                listings={listings}
                                                 tokensData={listings}
                                                 thingId={thing_id}
                                                 price={price!}
                                                 isConnected={isConnected}
                                             />
                                         ) : (
-                                            <MakeOffer
-                                                buy={buy}
-                                                isConnected={isConnected}
-                                                latestBid={listings[0]?.offers ? listings[0]?.offers[0]?.offer_price : 0}
-                                                bidder={listings[0]?.offers ? listings[0]?.offers[0]?.offered_by : "No bids yet"}
-                                                owner={listings[0]?.token.owner}
-                                            />
+                                            // <MakeOffer
+                                            //     buy={buy}
+                                            //     isConnected={isConnected}
+                                            //     latestBid={listings[0]?.offers ? listings[0]?.offers[0]?.offer_price : 0}
+                                            //     bidder={listings[0]?.offers ? listings[0]?.offers[0]?.offered_by : "No bids yet"}
+                                            //     owner={listings[0]?.token.owner}
+                                            // />
+                                            'great'
                                         )}
                                     </div>
                                 ) : (
                                     <div className="bg-primary-color text-white text-center w-fit rounded-lg p-3">Not Listed</div>
                                 )}
                             </div>
-                        </div>
                     </div>
                     {/* <SimilarNft /> */}
                 </div>
@@ -254,7 +234,7 @@ export default thing_id;
 
 export async function getServerSideProps({ params }: any) {
     const thing_id = params.thing_id;
-    let thingData:  MetadataByMetadataIdQueryResult | null | undefined = undefined
+    let thingData: MetadataByMetadataIdQueryResult | null | undefined = undefined;
 
     try {
         const { data, error } = await metadataByMetadataId(thing_id);
@@ -266,7 +246,7 @@ export async function getServerSideProps({ params }: any) {
     return {
         props: {
             thing_id,
-            data: thingData
+            data: thingData,
         },
     };
 }
