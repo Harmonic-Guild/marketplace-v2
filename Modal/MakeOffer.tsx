c10247d9af0e925277e0eb2b0a6397878b70164f
MZ Marshall <mzmarshall1278@gmail.com>
2023-07-19 23:58:11 +0100

    received bid info

--- Modal/MakeOffer.tsx
+++ Modal/MakeOffer.tsx
@@ -6,7 +6,7 @@ import { formatNearAmount } from "near-api-js/lib/utils/format";
 import styles from "../styles/MakeOffer.module.scss";
 import PlaceBid from "./PlaceBid";
 
-const MakeOffer = ({ buy, isConnected, latestBid, bidder, owner }: any) => {
+const MakeOffer = ({ buy, isConnected, bidInfo }: any) => {
     const [showModal, setShowModal] = useState(false);
     const [showNotConnectedModal, setShowNotConnectedModal] = useState(false);
 
@@ -32,15 +32,15 @@ const MakeOffer = ({ buy, isConnected, latestBid, bidder, owner }: any) => {
                 <div className="flex flex-col justify-center items-center w-full lg:w-1/2">
                     <div className="flex flex-wrap justify-center items-center gap-2 text-font-color">
                         <span className="font-bold text-lg">Latest bidder - </span>
-                        <span className=" text-lg">{bidder}</span>
+                        <span className=" text-lg">{bidInfo?.offers[0]?.offered_by || "No Bidder"}</span>
                     </div>
 
                     <div className="flex flex-row gap-2 mt-3 mb-3 lg:mb-0">
                         <div className="font-medium text-lg">Latest bid: </div>
                         <div className=" text-xl flex items-center gap-2">
-                            {latestBid ? (
+                            {bidInfo?.offers[0]?.offer_price ? (
                                 <div className="flex items-center gap-1">
-                                    {formatNearAmount(Number(latestBid || 0).toLocaleString("fullwide", { useGrouping: false }), 5)}
+                                    {formatNearAmount(Number(bidInfo?.offers[0]?.offer_price || 0).toLocaleString("fullwide", { useGrouping: false }), 5)}
                                     <Near className="w-4 h-4" fill="black" />
                                 </div>
                             ) : (
@@ -57,7 +57,7 @@ const MakeOffer = ({ buy, isConnected, latestBid, bidder, owner }: any) => {
                     </div>
 
                     <div className="text-center flex flex-col lg:flex-row justify-center items-center mt-3">
-                        <p className="font-bold">Owned by - @ {owner ? owner : "None"}</p>
+                        <p className="font-bold">Owned by - @ {bidInfo?.listed_by ? bidInfo?.listed_by : "None"}</p>
                         <div className="flex"></div>
                     </div>
                 </div>
